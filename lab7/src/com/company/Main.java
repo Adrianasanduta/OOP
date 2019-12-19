495fe69211b2d24952501c4d376176a806854bb6
AdrianaSanduta <sanduta.adriana9@gmail.com>
2019-12-19 21:14:10 +0200

    Add lab7

---
+++ lab7/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        GeometricBody cube = new Cube(3);
+        GeometricBody sphere = new Sphere(3);
+        GeometricBody parallelepiped = new Parallelepiped(1, 2, 3);
+
+        ArrayList<GeometricBody> bodies = new ArrayList<GeometricBody>();
+
+        bodies.add(cube);
+        bodies.add(sphere);
+        bodies.add(parallelepiped);
+
+        System.out.println("Biggest Surface = "+ GeometricBodyController.getBiggestSurfaceBody(bodies).getSurface());
+        System.out.println("Biggest Volume = "+ GeometricBodyController.getBiggestVolumeBody(bodies).getVolume());
+
+    }
+}
